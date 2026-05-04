95c4768fa4e3399b0c9818a45f6e937e8a78aa81
Vrushali Kottkona <vrushalimk9@gmail.com>
2026-05-04 17:51:37 +0530

    Initial commit

---
+++ src/main/java/com/Patient/PatientHealthcareApp/PatientHealthcareAppApplication.java
@@ -0,0 +1,13 @@
+package com.Patient.PatientHealthcareApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientHealthcareAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientHealthcareAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Patient/PatientHealthcareApp/PatientHealthcareAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Patient.PatientHealthcareApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientHealthcareAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
